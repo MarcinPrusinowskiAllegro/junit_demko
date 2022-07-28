fbc19838398b1012d478e5f0aab7f89cf9e52668
marcin.prusinowski <marcin.prusinowski@allegro.pl>
2022-07-28 11:20:20 +0200

    Aync fix

--- src/main/kotlin/com/example/demko_testy/item/domain/query/FindItemByIdQuery.kt
+++ src/main/kotlin/com/example/demko_testy/item/domain/query/FindItemByIdQuery.kt
@@ -1,15 +1,9 @@
 package com.example.demko_testy.item.domain.query
 
 import com.example.demko_testy.item.domain.Item
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import org.springframework.http.HttpHeaders
+import kotlinx.coroutines.*
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.awaitExchange
-import org.springframework.web.util.UriComponentsBuilder
-import reactor.core.publisher.Mono
-import java.net.URI
 
 interface FindItemByIdRepository {
     fun findById(id: Int): Item?
@@ -30,35 +24,46 @@ class FindItemByIdQuery(
 
     suspend fun execute(id: Int): Item? {
 
-        val result = webClient.get()
-            .uri(URI.create("http://localhost"))
-            .awaitExchange {
-                it.bodyToMono(String::class.java)
-            }
+//        val result = webClient.get()
+//            .uri(URI.create("http://localhost"))
+//            .awaitExchange {
+//                it.bodyToMono(String::class.java)
+//            }
 
-        CallResult(
-            first = firstCall(),
-            second = secondCall(),
-            third = thirdCall()
-        )
+        val result = coroutineScope {
 
+            val first = async { firstCall() }
+            val second = async { secondCall() }
+            val third = async { thirdCall() }
+
+            CallResult(
+                first = first.await(),
+                second = second.await(),
+                third = third.await()
+            )
+        }
+
+
+        println(result)
         return findItemByIdRepository.findById(id)
     }
 
 }
 
-
 suspend fun firstCall(): Int {
-    delay(1000)
+    delay(3000)
+    println("1")
     return 1
 }
 
 suspend fun secondCall(): Int {
-    delay(1000)
+    delay(2000)
+    println("2")
     return 2
 }
 
 suspend fun thirdCall(): Int {
     delay(1000)
+    println("3")
     return 3
 }
